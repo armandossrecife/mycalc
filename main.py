97a68a71f305903a80c5812aa929e2380cd49a93
Armando Soares Sousa <armando.sousa@gmail.com>
2023-08-10 17:53:51 +0000

    Criei operacoes basicas e ajustei o modulo principal para chamar as operacoes basicas.

--- main.py
+++ main.py
@@ -1,4 +1,29 @@
 import aritmetica
 
 class CalculadoraCientifica:
-    pass
+    def __init__(self) -> None:
+        self.operacoes_basicas = aritmetica.OperacoesBasicas()
+
+    def menu(self) -> None:
+        print('Mini Calculadora')
+        print(" Menu de Opções:                 ")
+        print(' 1 - Operações básicas           ')
+        print(' S - SAIR                        ')
+
+    def executar(self) -> None: 
+        while True:
+            self.menu()
+            opcao = input("Escolha uma opção: ")
+            if opcao == '1':
+                self.operacoes_basicas.executar()
+            elif opcao.lower() == 's':
+                break
+            else:
+                print("Opção inválida")
+            
+            continuar = input("Deseja continuar? (S/N): ")
+            if continuar.lower() != 's':
+                break
+
+calculadora = CalculadoraCientifica()
+calculadora.executar()
